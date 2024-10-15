d85c8f4da0ec0360d8983c41a52f7041c471fc6a
jm4rcos <joaomarcosmacimiro@gmail.com>
2024-10-14 22:25:51 -0300

    kubernetes services

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+  [Route("api/c/[controller]")]
+  [ApiController]
+  public class PlatformsController : ControllerBase
+  {
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+      Console.WriteLine("--> Inbound POST # Command Service");
+      return Ok("Inbound test of from Platforms Controller");
+    }
+  }
+}
